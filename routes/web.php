b25d90aa64679ae013a4389baf36f3b5699d9a94
xeryz05 <xierradepan01@gmail.com>
2024-06-19 14:28:48 +0700

    change route to resource

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\VpnController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/show-detail', function () {
-    return view('show-detail');
-});
+// Route::get('vpn',[VpnController::class, 'index']);
+
+Route::resource('vpn', VpnController::class)
+    ->only('index');
+
+    
+// Route::get('/', function () {
+//     return view('welcome');
+// });
